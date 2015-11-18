5fc2998c520f5e87d6d7dfae5baba5566e9bf331
Abhas Abhinav <abhas@deeproot.in>
2015-11-18 23:40:31 +0530

    Replace libvirtd gid using the value passed throught the env

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-if [ ! -f "/data/vm/webvirtmgr.sqlite3" ]; then
-
-chown webvirtmgr /var/run/libvirt/libvirt-sock
-/usr/bin/python /webvirtmgr/manage.py syncdb --noinput
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@localhost', '1234')" | /usr/bin/python /webvirtmgr/manage.py shell
-
+if [ ! -f "/data/vm/webvirtmgr.sqlite3" ]; 
+then
+  sed -i s/108/$LIBVIRTGID/g /etc/passwd
+  sed -i s/108/$LIBVIRTGID/g /etc/group
+  /usr/bin/python /webvirtmgr/manage.py syncdb --noinput
+  echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@localhost', '1234')" | /usr/bin/python /webvirtmgr/manage.py shell
 fi
 
